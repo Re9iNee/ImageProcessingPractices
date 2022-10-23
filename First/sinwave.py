e46aa31f74f0693a40f52b7e17fb40265d05d994
Re9iNee <attarzadeh76@gmail.com>
2022-10-23 21:20:41 +0330

    feature-making sine wave

---
+++ First/sinwave.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+import matplotlib.pyplot as plot
+
+
+def makeSineWave(waveLength, frequency):
+    time = np.arange(0, waveLength, 0.1) * frequency
+
+    amplitude = np.sin(time)
+
+    plot.plot(time, amplitude)
+
+    def showWave(wave):
+        wave.title('Sine Wave')
+        wave.xlabel('Time')
+        wave.ylabel('X')
+        wave.grid(True, which='both')
+        wave.axhline(y=0, color='k')
+
+        wave.show()
+
+    showWave(plot)
+
+    return plot
+
+
+simpleSineWave = makeSineWave((np.pi * 6), (np.pi / 4))
+
+
+def findMax():
+    print("TODO")
+
+
+findMax()
