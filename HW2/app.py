b0433af37af1d1168e3d4988bd458be436410b2a
Re9iNee <attarzadeh76@gmail.com>
2022-11-06 09:24:40 +0330

    Feature-ShowOriginalImage and converted Image together

--- HW2/app.py
+++ HW2/app.py
@@ -2,10 +2,34 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 
-# Import image and convert it into Unsigned integer then show it
-image = mpimg.imread('image.jpg')
-convertedImage = image.astype(np.uint8)
+ROWS = 2
+COLUMNS = 2
 
-imgplot = plt.imshow(convertedImage)
+# create a figure
+fig = plt.figure(figsize=(10, 7))
+
+
+# import the image
+originalImage = mpimg.imread('image.jpg')
+
+
+# convert image into Unsigned integer then show it
+convertedImage = originalImage.astype(np.uint8)
+
+# Adds a subplot at the 1st position
+fig.add_subplot(ROWS, COLUMNS, 1)
+# showing original image
+plt.imshow(originalImage)
+plt.axis('off')
+plt.title('Original Image')
+
+
+# Adds a subplot at the 2nd position
+fig.add_subplot(ROWS, COLUMNS, 2)
+# showing converted image
+plt.imshow(convertedImage)
+plt.axis('off')
+plt.title('Converted Image')
+
 
 plt.show()
