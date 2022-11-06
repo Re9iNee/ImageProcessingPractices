827a8477469ae0ee295bde5e2db0ee50bac203fb
Re9iNee <attarzadeh76@gmail.com>
2022-11-06 11:01:37 +0330

    Feature-Histogram using numpy lib

--- HW2/app.py
+++ HW2/app.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 
-ROWS = 1
+ROWS = 2
 COLUMNS = 2
 
 # create a figure
@@ -33,15 +33,15 @@ plt.title('Converted Image')
 
 
 # # Adds another subplot at the 3rd position
-# fig.add_subplot(ROWS, COLUMNS, 3)
+fig.add_subplot(ROWS, COLUMNS, 3)
 
 # Write histogram for converted image
-histogram, bin_edges = np.histogram(convertedImage, bins=256, range=(0, 1))
-plt.figure()
+histogram, bin_edges = np.histogram(convertedImage.ravel(), 256, [0, 256])
 plt.title("Grayscale Histogram")
 plt.xlabel("grayscale value")
 plt.ylabel('pixel count')
-plt.xlim([0.0, 1.0])
+plt.xlim([0, 256])
+
 
 plt.plot(bin_edges[0:-1], histogram)
 
