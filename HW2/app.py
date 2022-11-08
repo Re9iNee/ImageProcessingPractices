c843c4dbf47cbdc75b2401ce440b1cd26023b263
Re9iNee <attarzadeh76@gmail.com>
2022-11-08 23:48:14 +0330

    Feature-Python-calculating histogram

--- HW2/app.py
+++ HW2/app.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 
+
 ROWS = 2
 COLUMNS = 2
 
@@ -44,6 +45,31 @@ plt.xlim([0, 256])
 
 
 plt.plot(bin_edges[0:-1], histogram)
+
+
+# initialize the array to store histogram
+myHistogram = np.zeros(256, np.uint8)
+
+# get the row and column of the image
+row, col = convertedImage.shape[0], convertedImage.shape[1]
+
+print(row, col)
+
+# creates the histogram of the image
+for i in range(0, int(row / 8)):
+    for j in range(0, int(col / 8)):
+        myHistogram[convertedImage[i, j]] += 1
+
+# Adds the last subplot at the 4th position
+fig.add_subplot(ROWS, COLUMNS, 4)
+
+plt.title("My Grayscale Histogram")
+plt.xlabel("grayscale value")
+plt.ylabel('pixel count')
+plt.xlim([0, 256])
+
+
+plt.plot(myHistogram)
 
 
 plt.show()
